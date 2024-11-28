abb627281d02a8b31425cc5c05578a604670a60c
R4MB3B0Y <kingindoslayer@gmail.com>
2024-11-28 11:05:46 +0700

    Update helper.js

--- app/src/utils/helper.js
+++ app/src/utils/helper.js
@@ -1,5 +1,11 @@
 export const banner = `
 
+const banner = figlet.textSync("Kaisar Zero", {
+    font: "ANSI Shadow",
+    horizontalLayout: "default",
+    verticalLayout: "default",
+    width: 150,
+  });
             Running Kaisar Node Beta Extensions Cli Version                
                          Author : Nofan Rambe
                          Welcome & Enjoy Sir!
